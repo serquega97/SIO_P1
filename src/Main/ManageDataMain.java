4ba2c97e514847faf442922c9dc85c271c56ef63
Sergi Quevedo Garreta <sergiquevedogarreta@MacBook-Air-de-Sergi.local>
2020-03-09 17:59:32 +0100

    sergi:queries

--- src/Main/ManageDataMain.java
+++ src/Main/ManageDataMain.java
@@ -3,7 +3,10 @@ package Main;
 public class ManageDataMain {
 
 	public static void main(String[] args) {
-		
+		final String allUsers="SELECT * FROM User";
+		final String allRest="SELECT * FROM Restaurant";
+		final String avgAllGrades="SELECT AVG(grade) AS avg_grades FROM Relation";
+		final String avgUsers="SELECT * FROM User";
 	}
 
 }
